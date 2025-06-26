e0649a2b7e02d91aaeda59806667150959773cf0
Rupali Bhatnagar <rupalibhatnagar0509@gmail.com>
2025-06-26 20:38:59 +0530

    Update with trackers

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,26 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([ <
+        script
+        key = "hotjar-init"
+        dangerouslySetInnerHTML = {
+            {
+                __html: `
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6447027,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+            }
+        }
+        />
+    ]);
+};
